db89ed64308d916f83160d792d23215461993d7e
Kir93 <57649268+Kir93@users.noreply.github.com>
2019-11-13 11:17:00 +0900

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,9 +1,9 @@
 data = list(map(int,input().split()))
 
 for i in range(len(data)):
-  for j in range(len(data)):
-    if data[i] < data[j]:
-      data[i], data[j] = data[j], data[i]
+  for j in range(1,len(data)-i):
+    if data[j-1] > data[j]:
+      data[j-1], data[j] = data[j], data[j-1]
 
 for i in data:
   print(i, end=' ')
