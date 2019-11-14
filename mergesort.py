c7ba1383e17c87e1064c347ec0413c5ef1d2db80
Kir93 <57649268+Kir93@users.noreply.github.com>
2019-11-14 10:14:26 +0900

    Update mergesort.py

--- mergesort.py
+++ mergesort.py
@@ -1,39 +1,41 @@
 import sys
 
-def merge_sorted(arr):
-    if len(arr)>1:
-        mid = len(arr)//2
-        left = arr[:mid]
-        right = arr[mid:]
+def merge_sort(data):
+  if len(data) > 1:
 
-        l = merge_sorted(left)
-        r = merge_sorted(right)
-        return merge(l, r)
-    else:
-        return arr
-        
+    mid = len(data)//2
+    left = data[:mid]
+    right = data[mid:]
+
+    l = merge_sort(left)
+    r = merge_sort(right)
+    return merge(l, r)
+
+  else:
+    return data
+
 def merge(left, right):
-    i = 0
-    j = 0
-    arr = []
-    
-    while (i<len(left)) & (j<len(right)):
-        if left[i] < right[j]:
-            arr.append(left[i])
-            i+=1
-        else:
-            arr.append(right[j])
-            j+=1
-    #ㅇㅇㅇ
-    while (i<len(left)):
-        arr.append(left[i])
-        i+=1
-    #
-    while (j<len(right)):
-        arr.append(right[j])
-        j+=1
-        
-    return arr
+  i=0
+  j=0
+  sort_data = []
 
-arr = list(map(int,sys.stdin.readline().split()))
-print(merge_sorted(arr))
+  while (i<len(left)) & (j<len(right)):
+    if left[i] < right[j]:
+      sort_data.append(left[i])
+      i+=1
+    else:
+      sort_data.append(right[j])
+      j+=1
+  
+  while (i<len(left)):
+    sort_data.append(left[i])
+    i+=1
+  while (j<len(right)):
+    sort_data.append(right[j])
+    j+=1
+  return sort_data
+
+data = list(map(int,sys.stdin.readline().split()))
+
+for i in merge_sort(data):
+  print(i,end=' ')
