0f161802200399294c809f90cbf624573abaee4f
Kir93 <57649268+Kir93@users.noreply.github.com>
2019-11-15 10:51:43 +0900

    Update heapsort.py

--- heapsort.py
+++ heapsort.py
@@ -1,20 +1,24 @@
-def heap_sort(a):
-    size = len(a)
-    for i in range(size, -1, -1):
-      heap_heapify(a, size, i)
-    for i in range(size-1, 0, -1):
-      a[i], a[0] = a[0], a[i]
-      heap_heapify(a, i, 0)
-def heap_heapify(a, size, i):
-    largest = i
-    L = 2*i + 1
-    R = 2*i + 2
-    if L<size and a[i]<a[L]: largest = L
-    if R<size and a[largest]<a[R]: largest = R
-    if largest != i:
-        a[i], a[largest] = a[largest], a[i]
-        heap_heapify(a, size, largest)
-arr = [9,1,22,4,0,-1,1,22,100,10]
-heap_sort(arr)
-print( arr )
-# [-1, 0, 1, 1, 4, 9, 10, 22, 22, 100]
+import sys
+# 힙 정렬
+def heap_sort(data):
+  size = len(data)
+  for i in range(size, -1, -1):
+    heap_heapify(data,size,i)
+  for i in range(size-1,0,-1):
+    data[i], data[0] = data[0], data[i]
+    heap_heapify(data,i,0)
+# 힙을 구성
+def heap_heapify(data, size, i):
+  large = i
+  l = 2*i + 1
+  r = 2*i + 2
+  if l<size and data[i]<data[l]:  large = l
+  if r<size and data[large]<data[r]:  large = r
+  if large != i:
+    data[i], data[large] = data[large], data[i]
+    heap_heapify(data,size,large)
+data = list(map(int,sys.stdin.readline().split()))
+heap_sort(data)
+
+for i in data:
+  print(i,end=' ')
