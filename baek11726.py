96d8d1d4ea65d875ebce1750d5dc38857c66940f
Kir93 <57649268+Kir93@users.noreply.github.com>
2019-11-19 16:53:20 +0900

    Create baek11726.py

---
+++ baek11726.py
@@ -0,0 +1,14 @@
+import sys
+def dp(n):
+  val = [0]
+  for i in range(1,n+1):
+    if i == 1:
+      val.append(1)
+    elif i == 2:
+      val.append(2)
+    else:
+      val.append(val[i-1] + val[i-2])
+  return val[n]
+
+n = int(sys.stdin.readline())
+print(dp(n)%10007)
