404ddc57102c579d1df027f3eeb3827908542056
Kir93 <57649268+Kir93@users.noreply.github.com>
2019-11-21 17:35:53 +0900

    다이나믹 프로그래밍 피보나치

--- dyn_fibonacci.py
+++ dyn_fibonacci.py
@@ -1,1 +1,12 @@
+import sys
+def fibonacci(n):
+  val = [0]
+  for i in range(1,n+1):
+    if i==1:  val.append(1)
+    elif i==2:  val.append(1)
+    else:
+      val.append(val[i-1]+val[i-2])
+  return val[n]
 
+n = int(sys.stdin.readline())
+print(fibonacci(n))
