4e85dafa8dc56433d7afbe93bbd656b28323eaf0
Kir93 <57649268+Kir93@users.noreply.github.com>
2019-11-29 12:18:47 +0900

    섹션1) 6. 자릿수의 합

--- s1q6.py
+++ s1q6.py
@@ -10,10 +10,33 @@ def digit_sum(x):
 n = int(input())
 data = list(input().split())
 MAX = -99999
-for idx, i in enumerate(data):
+for i in data:
     temp = digit_sum(i)
     if MAX < temp:
         MAX = temp
-        num = idx
+        num = i
 
-print(data[num])
+print(i)
+
+
+'''
+import sys
+sys.stdin=open("input.txt", "r")
+def digit_sum(x):
+    sum=0
+    while x>0:
+        sum+=x%10
+        x=x//10
+    return sum
+
+n=int(input())
+a=list(map(int, input().split()))
+res=0
+max=-2147000000
+for x in a:
+    tot=digit_sum(x)
+    if tot>max:
+        max=tot
+        res=x
+print(res)
+'''
