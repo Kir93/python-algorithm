c6d0a3bdab1bb3064d095338f1d04030bc874756
Kir93 <57649268+Kir93@users.noreply.github.com>
2019-12-02 17:10:26 +0900

    섹션2) 4. 두 리스트 합치기

--- s2q4.py
+++ s2q4.py
@@ -1,11 +1,50 @@
+#my code
 import sys
-#sys.stdin=open("input.txt","rt")
+sys.stdin=open("input.txt","rt")
 n = int(input())
 data1 = list(map(int,input().split()))
-n = int(input())
+m = int(input())
 data2 = list(map(int,input().split()))
-data = data1 + data2
-data.sort()
+data = []
+p1, p2 = 0, 0
+while(True):
+  if p1==n:
+    for i in range(p2,len(data2)):
+      data.append(data2[i])
+    break
+  elif p2==m:
+    for i in range(p1,len(data1)):
+      data.append(data1[i])
+    break
+  if data1[p1]<=data2[p2]:
+    data.append(data1[p1])
+    p1+=1
+  else:
+    data.append(data2[p2])
+    p2+=1
 
 for x in data:
   print(x, end=' ')
+  
+#best code
+import sys
+sys.stdin=open("input.txt", "r")
+n=int(input())
+a=list(map(int, input().split()))
+m=int(input())
+b=list(map(int, input().split()))
+p1=p2=0
+c=[]
+while p1<n and p2<m:
+    if a[p1]<b[p2]:
+        c.append(a[p1])
+        p1+=1
+    else:
+        c.append(b[p2])
+        p2+=1
+if p1<n:
+    c=c+a[p1:]
+if p2<m:
+    c=c+b[p2:]
+for x in c:
+    print(x, end=' ')
