a3d472f5b85467e9a715c7b7e98a84281f11fc2a
Kir93 <57649268+Kir93@users.noreply.github.com>
2019-12-04 00:07:43 +0900

    섹션2) 7. 사과나무

---
+++ s2q7.py
@@ -0,0 +1,17 @@
+import sys
+#sys.stdin = open("input.txt", "rt")
+n = int(input())
+data = [list(map(int,input().split())) for _ in range(n)]
+s=e=n//2
+r = 0
+for i in range(n):
+    for j in range(s,e+1):
+        r += data[i][j]
+    if i<n//2:
+        s -= 1
+        e += 1
+    else:
+        s += 1
+        e -= 1
+
+print(r)
