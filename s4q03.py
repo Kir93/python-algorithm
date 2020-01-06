7b94fcdd9c52fe2a83dfd7d8ee1b6fff25a875f1
Kir93 <57649268+Kir93@users.noreply.github.com>
2020-01-06 23:40:31 +0900

    섹션4) 후위표기식 만들기

---
+++ s4q03.py
@@ -0,0 +1,29 @@
+import sys
+#sys.stdin = open("input.txt", "r")
+s = input()
+stack = []
+res = ''
+
+for x in s:
+    if x.isdecimal():
+        res += x
+    else:
+        if x == '(':
+            stack.append(x)
+        elif x == '*' or x == '/':
+            while stack and (stack[-1] == '*' or stack[-1] == '/'):
+                res += stack.pop()
+            stack.append(x)
+        elif x == '+' or x == '-':
+            while stack and stack[-1] != '(':
+                res += stack.pop()
+            stack.append(x)
+        elif x == ')':
+            while stack and stack[-1] != '(':
+                res += stack.pop()
+            stack.pop()
+
+while stack:
+    res += stack.pop()
+
+print(res)
