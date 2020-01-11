137e751a84a6a1aa724f781c994dc95d55e2bd11
Kir93 <57649268+Kir93@users.noreply.github.com>
2020-01-12 01:14:48 +0900

    섹션4) 11. 최대힙

---
+++ s4q11.py
@@ -0,0 +1,15 @@
+import sys
+import heapq as hq
+#sys.stdin = open("input.txt", "r")
+hip = []
+while True:
+    n = int(input())
+    if n == -1:
+        break
+    elif n == 0:
+        if len(hip) == 0:
+            print(-1)
+        else:
+            print(hq.heappop(hip)[1])
+    else:
+        hq.heappush(hip, (-n, n))
