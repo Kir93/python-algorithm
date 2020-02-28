ee5ac05a3a18faa23f048f6c0249116e5ad06932
Kir93 <57649268+Kir93@users.noreply.github.com>
2020-02-29 01:18:55 +0900

    섹션1) 5. 정다면체

--- s1q05.py
+++ s1q05.py
@@ -15,3 +15,14 @@ for i in range(len(cnt)):
     if MAX == cnt[i]:
         print(i, end=' ')
 
+#new mycode
+import sys
+#sys.stdin = open("input.txt", "r")
+n, m = map(int, input().split())
+cnt = [0] * (n+m+3)
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        cnt[i+j] += 1
+for x in range(len(cnt)):
+    if(max(cnt) == cnt[x]):
+        print(x, end=" ")
