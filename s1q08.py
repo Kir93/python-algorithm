e0c81a9a750c9ae2f206b76ef1900d590dfd0ef4
Kir93 <57649268+Kir93@users.noreply.github.com>
2020-03-03 00:23:28 +0900

    섹션 1) 8. 뒤집힌 소수
    문자열이 아닌 그냥 숫자로 뒤집은 수를 찾을 때에는
    while x > 0:
            t = x%10
            res = res*10+t
            x=x//10
    이 방법을 이용한다.
    소수인지 구할때는 x(판별 수)//2+1를 해 검사수를 줄인다.
    (절반까지 판별하면 이미 소수 판별 끝남)

--- s1q08.py
+++ s1q08.py
@@ -24,3 +24,29 @@ for x in data:
     tmp = reverse(x)
     if isPrime(tmp):
         print(tmp, end=' ')
+
+#new mycode
+
+import sys
+#sys.stdin = open('input.txt', 'rt')
+
+
+def isPrime(x):
+    newN = ''
+    for i in range(1, len(x)+1):
+        newN += x[-i]
+    newN = int(newN)
+    if(newN == 1):
+        return False
+    for j in range(2, newN):
+        if(newN % j == 0):
+            return False
+    return newN
+
+
+n = int(input())
+data = list(input().split())
+for x in data:
+    r = isPrime(x)
+    if(r != False):
+        print(r, end=' ')
