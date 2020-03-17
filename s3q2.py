c36058d63c4678544e0b07e605744341ed8d0091
Kir93 <57649268+Kir93@users.noreply.github.com>
2020-03-17 11:57:36 +0900

    섹션3) 2. 랜선 자르기
    if문으로 걸러줄 필요없었음.
    어차피 lt가 올라가기 때문에 무조건 값이 올라감

--- s3q2.py
+++ s3q2.py
@@ -18,5 +18,29 @@ while lt<=rt:
         lt = mid + 1
     else:
         rt = mid - 1
-
 print(res)
+
+#new mycode
+import sys
+#sys.stdin=open("input.txt","rt")
+n, m = map(int, input().split())
+data = []
+for _ in range(n):
+  data.append(int(input()))
+data.sort()
+lt= 1
+rt = data[n-1]
+r = -999999
+while lt<=rt:
+  s = 0
+  mid= (lt+rt) // 2
+  for x in data:
+    s += x // mid
+  if s < m:
+    rt = mid -1
+  elif s >= m:
+    r = mid
+    lt = mid + 1
+  elif s > m:
+    lt = mid + 1
+print(r)
