385ba02a7c65c5571e59f79c8d31901151d7fef2
Kir93 <57649268+Kir93@users.noreply.github.com>
2020-03-17 12:03:16 +0900

    섹션3) 2. 랜선자르기
    if문으로 한번 더 걸러줄 필요없음
    어차피 lt는 기존 값보다 더 크기 떄문

--- s3q2.py
+++ s3q2.py
@@ -20,6 +20,34 @@ while lt<=rt:
         rt = mid - 1
 print(res)
 
+#best code
+import sys
+sys.stdin=open("input.txt", "r")
+def Count(len):
+    cnt=0
+    for x in Line:
+        cnt+=(x//len)
+    return cnt
+
+k, n=map(int, input().split())
+Line=[]
+res=0
+largest=0
+for i in range(k):
+    tmp=int(input())
+    Line.append(tmp)
+    largest=max(largest, tmp)
+lt=1
+rt=largest
+while lt<=rt:
+    mid=(lt+rt)//2
+    if Count(mid)>=n:
+        res=mid
+        lt=mid+1
+    else:
+        rt=mid-1
+print(res)
+
 #new mycode
 import sys
 #sys.stdin=open("input.txt","rt")
@@ -40,7 +68,5 @@ while lt<=rt:
     rt = mid -1
   elif s >= m:
     r = mid
-    lt = mid + 1
-  elif s > m:
     lt = mid + 1
 print(r)
