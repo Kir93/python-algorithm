236210b31991348d7cef6e7ea4a7a5dc0b8b6411
Kir93 <57649268+Kir93@users.noreply.github.com>
2020-03-27 05:32:53 +0900

    섹션3) 7. 창고 정리

--- s3q07.py
+++ s3q07.py
@@ -10,3 +10,16 @@ for _ in range(m):
     data.sort()
 
 print(data[-1] - data[0])
+
+#new mycode
+import sys
+#sys.stdin = open('input.txt', 'rt')
+n = int(input())
+data = list(map(int, input().split()))
+m = int(input())
+data.sort()
+for _ in range(m):
+    data[0] += 1
+    data[-1] -= 1
+    data.sort()
+print(data[-1]-data[0])
