c38fd914acf9c578a75062ffcb15341c0d7ca7cb
Kir93 <57649268+Kir93@users.noreply.github.com>
2020-03-26 17:38:21 +0900

    섹션3) 2. 랜선자르기
    무언가를 결정할 경우에는 lt, rt mid를 활용하면 가장 쉽고 빠르게 구할 수 있다.

--- s3q02.py
+++ s3q02.py
@@ -50,23 +50,24 @@ print(res)
 
 #new mycode
 import sys
-#sys.stdin=open("input.txt","rt")
-n, m = map(int, input().split())
+#sys.stdin = open('input.txt', 'rt')
+k, n = map(int, input().split())
 data = []
-for _ in range(n):
-  data.append(int(input()))
-data.sort()
-lt= 1
-rt = data[n-1]
+for _ in range(k):
+    data.append(int(input()))
+lt = 1
+rt = max(data)
 r = -999999
-while lt<=rt:
-  s = 0
-  mid= (lt+rt) // 2
-  for x in data:
-    s += x // mid
-  if s < m:
-    rt = mid -1
-  elif s >= m:
-    r = mid
-    lt = mid + 1
+while lt <= rt:
+    mid = (lt + rt) // 2
+    cnt = 0
+    for x in data:
+        cnt += x // mid
+    if cnt >= n and mid > r:
+        r = mid
+        lt = mid + 1
+    else:
+        rt = mid - 1
+
 print(r)
+
