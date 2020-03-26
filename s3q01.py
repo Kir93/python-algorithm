090c74655284132fa9cc5c4be466e047bed423e1
Kir93 <57649268+Kir93@users.noreply.github.com>
2020-03-26 17:25:16 +0900

    섹션3) 1. 이분검색
    주어진 것 중 가까운 것, 같은 것을 찾는 것은 lt rt를 활용한 이분검색이 빠르다.

--- s3q01.py
+++ s3q01.py
@@ -14,3 +14,21 @@ while lt<=rt:
     rt=mid-1
   else:
     lt=mid+1
+
+#new mycode
+import sys
+#sys.stdin = open('input.txt', 'rt')
+n, m = map(int, input().split())
+data = list(map(int, input().split()))
+data.sort()
+lt = 0
+rt = n-1
+while lt <= rt:
+    mid = (lt + rt) // 2
+    if data[mid] > m:
+        rt = mid-1
+    elif data[mid] == m:
+        print(mid+1)
+        break
+    elif data[mid] < m:
+        lt = mid + 1
