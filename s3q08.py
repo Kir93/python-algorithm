70b655691cbed6fb63af185af39bdf17afc1fd61
Kir93 <57649268+Kir93@users.noreply.github.com>
2020-03-27 05:44:46 +0900

    섹션3) 8. 침몰하는 타이타닉
    왼쪽 오른쪽 모두 팝을 해야되는 경우 데크로 변경해서 하는 것이 더 효율적
    from collections import deque

--- s3q08.py
+++ s3q08.py
@@ -18,3 +18,23 @@ while data:
     data.popleft()
     cnt+=1
 print(cnt)
+
+#new mycode
+import sys
+#sys.stdin = open('input.txt', 'rt')
+n, m = map(int, input().split())
+data = list(map(int, input().split()))
+data.sort()
+cnt = 0
+while data:
+    if len(data) == 1:
+        cnt += 1
+        break
+    if data[-1] + data[0] > m:
+        cnt += 1
+        data.pop()
+    elif data[-1] + data[0] <= m:
+        cnt += 1
+        data.pop(0)
+        data.pop()
+print(cnt)
