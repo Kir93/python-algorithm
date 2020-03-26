44952f9d3e9e859f27ae07f3c39d65749ea0ecb3
Kir93 <57649268+Kir93@users.noreply.github.com>
2020-03-26 18:05:16 +0900

    섹션3) 3. 뮤직비디오

--- s3q03.py
+++ s3q03.py
@@ -24,3 +24,28 @@ while(lt<=rt):
         rt = mid - 1
 
 print(res)
+
+#new mycode
+import sys
+#sys.stdin = open('input.txt', 'rt')
+n, m = map(int, input().split())
+data = list(map(int, input().split()))
+lt = 1
+rt = sum(data)
+res = -999999
+while lt <= rt:
+    mid = (lt + rt) // 2
+    cnt = 0
+    r = 0
+    for x in data:
+        if mid > r + x:
+            r += x
+        else:
+            cnt += 1
+            r = x
+    if cnt >= m:
+        res = mid
+        lt = mid + 1
+    else:
+        rt = mid - 1
+print(res)
