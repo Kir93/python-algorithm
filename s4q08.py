15eacbaacf92bf88c85b5f94fbcc79a616d5e67e
Kir93 <57649268+Kir93@users.noreply.github.com>
2020-04-03 11:46:37 +0900

    섹션4) 8. 단어찾기
    같은 단어등을 찾는 문제의 경우 dict() 딕셔너리를 이용하면 편하게 할 수 있다.
    딕셔너리는 key값을 문자열로도 가질 수 있기 때문이다.
    불러올 때는
    for key, val in data.items(): 을 이용하면 key val을 모두 가지고 올 수 있다.

--- s4q08.py
+++ s4q08.py
@@ -15,3 +15,29 @@ for key, val in data.items():
   if val == 1:
     print(key)
     break
+
+#new mycode
+import sys
+from collections import deque
+#sys.stdin = open("input.txt", "rt")
+n = int(input())
+rd = []
+sd = []
+for i in range(n):
+  rd.append(input())
+for i in range(n-1):
+  sd.append(input())
+while rd:
+  rd = deque(rd)
+  if len(rd) == 1:
+    print(rd[0])
+    break
+  c = rd.popleft() 
+  for x in sd:
+    if c != x:
+      rd.append(c)
+    else:
+      break
+  else:
+    print(c)
+    break
