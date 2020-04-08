a0bffc7726aa5f43eb4d29269b9fecaaf2a6dec8
Kir93 <57649268+Kir93@users.noreply.github.com>
2020-04-08 11:03:58 +0900

    섹션4) 2. 쇠막대기

--- s4q02.py
+++ s4q02.py
@@ -14,3 +14,21 @@ for i in range(len(s)):
             cnt += 1
 
 print(cnt)
+
+#new mycode
+import sys
+#sys.stdin = open("input.txt", "rt")
+laser = input()
+bar = []
+res = 0
+for i in range(len(laser)):
+  if laser[i] == '(':
+    bar.append(laser[i])
+  else:
+    if laser[i-1] == '(':
+      bar.pop()
+      res += len(bar)
+    else:
+      bar.pop()
+      res += 1
+print(res)
