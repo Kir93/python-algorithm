a1ab47310a8b52fe64e801e36eb4b57f7344b6fc
Kir93 <57649268+Kir93@users.noreply.github.com>
2020-04-08 11:33:24 +0900

    섹션4) 3. 후위표현식 만들기
    x.isdecimal()을 이용하면 숫자인지 여부 알 수있음.

--- s4q03.py
+++ s4q03.py
@@ -27,3 +27,31 @@ while stack:
     res += stack.pop()
 
 print(res)
+
+#new mycode
+import sys
+#sys.stdin = open("input.txt", "rt")
+data = input()
+d = []
+r = ''
+for x in data:
+  if x == '+' or x == '-':
+    while d and d[-1] != '(':
+      r += d.pop()
+    d.append(x)
+  elif x == '*' or x == '/':
+    while d and (d[-1] == '*' or d[-1] == '/'):
+      r += d.pop()
+    d.append(x)
+  elif x == '(':
+    d.append(x)
+  elif x == ')':
+    while d and d[-1] != '(':
+      r += d.pop()
+    d.pop()
+  else:
+    r += x
+
+while d:
+  r += d.pop()
+print(r)
