9163ac6334ae92118c86fe1d31759d5110ee1e51
Kir93 <57649268+Kir93@users.noreply.github.com>
2020-06-09 13:48:31 +0900

    Update s5q03.py

--- s5q03.py
+++ s5q03.py
@@ -7,7 +7,7 @@ def DFS(x):
         for i in range(x):
             if ck[i] == 1:
                 print(i, end=" ")
-        print()S
+        print()
     else:
         ck[x] = 1
         DFS(x+1)
@@ -19,3 +19,29 @@ if __name__ == "__main__":
     n = int(input())
     ck = [0 for i in range(n+1)]
     DFS(1)
+
+#new code
+import sys
+sys.stdin = open("input.txt", "rt")
+
+
+def DFS(n):
+    if n > x:
+        for s in r:
+            print(s, end=' ')
+        print()
+        return
+    else:
+        r.append(n)
+        DFS(n+1)
+        print(r)
+        r.pop()
+        DFS(n+1)
+        print(r)
+
+
+if __name__ == "__main__":
+    r = []
+    x = int(input())
+    DFS(1)
+
