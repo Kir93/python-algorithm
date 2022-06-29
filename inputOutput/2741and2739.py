9ea755727b58d6a221d743ddfa1ced5dd84f324e
Kir93 <kir931028@gmail.com>
2022-06-29 16:41:33 +0900

    update 입출력 문제 풀기

---
+++ inputOutput/2741and2739.py
@@ -0,0 +1,12 @@
+# 2741
+for i in range(1,int(input())+1):
+    print(i)
+
+# 2742
+for i in reversed(range(1, int(input())+1)):
+    print(i)
+
+# 2739
+n = int(input())
+for i in range(1,10):
+    print(f'{n} * {i} = {n*i}')
