65f0a60e5410b7bd63e9c47093535c4c0b4491a0
Kir93 <kir931028@gmail.com>
2022-06-30 09:53:23 +0900

    update 입출력 문제 풀기

---
+++ inputOutput/1924and.py
@@ -0,0 +1,27 @@
+# 1924
+# x,y = map(int, input().split())
+#monthList = [31,28,31,30,31,30,31,31,30,31,30,31]
+# dayList = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+# Day = 0
+
+# for i in range(x-1):
+#     Day += monthList[i]
+
+# print(dayList[(Day+y)%7])
+
+# 1924-2
+# import calendar
+# dayList = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT','SUN']
+
+# print(dayList[calendar.weekday(2007,x,y)])
+
+# 8393
+# result = 0
+# for i in range(1, int(input())+1):
+#     result+=i
+# print(result)
+
+#10818
+# int(input())
+# numbers = list(map(int,input().split()))
+# print(min(numbers),max(numbers))
