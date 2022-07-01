0ea48e12f856f6700644e0ec6dd57bf85b707453
Kir93 <kir931028@gmail.com>
2022-07-01 10:16:58 +0900

    update 입출력 문제 풀기

---
+++ inputOutput/2438and2442.py
@@ -0,0 +1,24 @@
+# 2438
+# n = int(input())
+# for i in range(1,n+1):
+#     print('*'*i)
+
+# 2439
+# n = int(input())
+# for i in reversed(range(n)):
+#     print(f"{' '*i}{'*'*(n-i)}")
+
+# 2440
+# n = int(input())
+# for i in reversed(range(1,n+1)):
+#     print('*'*i)
+
+# 2441
+# n = int(input())
+# for i in reversed(range(1,n+1)):
+#     print(f"{' '*(n-i)}{'*'*i}")
+
+# 2442
+# n = int(input())
+# for i in reversed(range(n)):
+#     print(f"{' '*i}{'*'*(2*(n-i)-1)}")
