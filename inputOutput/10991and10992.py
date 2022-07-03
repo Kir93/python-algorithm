c41f64cbee4f5dde75c70c8a417e597631335349
Kir93 <kir931028@gmail.com>
2022-07-03 12:00:41 +0900

    update 입출력 문제풀이

---
+++ inputOutput/10991and10992.py
@@ -0,0 +1,14 @@
+# 10991
+# n = int(input())
+# for i in reversed(range(n)):
+#     print(f"{' '*i}{'* '*(n-i)}")
+
+# 10992
+# n = int(input())
+# for i in range(n):
+#     if i == 0:
+#         print(f"{' '*(n-1)}{'*'}")
+#     elif i == n-1:
+#         print('*'*(2*i+1))
+#     else:
+#         print(f"{' '*(n-i-1)}{'*'}{' '*(2*(i-1)+1)}{'*'}")
