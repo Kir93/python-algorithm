d4bf75f61ba6d8384b854ef133a6696341713e07
Kir93 <kir931028@gmail.com>
2022-07-06 14:44:59 +0900

    update 11727

--- dp/11727.py
+++ dp/11727.py
@@ -1,15 +1,7 @@
 n = int(input())
-m = 1001
-if n < 1001: m = n
-dp = [0] * (m+1)
-
-if m == 1: print(1)
-elif m == 2: print(3)
+dp = [0,1,3]
+if n==1 or n==2: print(dp[n])
 else:
-    for i in range(1, m+1):
-        if i==1: dp[i] = 1
-        elif i==2: dp[i] = 3
-        else:
-            dp[i] = 2*dp[i-2] + dp[i-1]
-
-    print(dp[m]%10007)
+    for i in range(3, n+1):
+        dp.append(dp[i-1]+2*dp[i-2])
+    print(dp[n]%10007)
