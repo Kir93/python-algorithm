a52d1aa95df5e78698433409561604db782615d7
Kir93 <kir931028@gmail.com>
2022-07-06 15:08:07 +0900

    update 11727

--- dp/11727.py
+++ dp/11727.py
@@ -1,3 +1,4 @@
-dp = [0,1,3]
-for i in range(3, 1001): dp.append(2*dp[i-2] + dp[i-1])
-print(dp[int(input())]%10007)
+n = int(input())
+dp = [1] * (n+1)
+for i in range(2, n+1): dp[i] = 2*dp[i-2] + dp[i-1]
+print(dp[n]%10007)
