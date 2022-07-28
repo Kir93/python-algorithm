574cb632e98c931b5709dc5afcdd1c1b5986f6e6
Kir93 <kir931028@gmail.com>
2022-07-28 20:59:58 +0900

    update new code

--- dp/1699.py
+++ dp/1699.py
@@ -1,8 +1,7 @@
-n = int(input())
-s = [x for x in range(1, 317)]
-dp = [i for i in range(n+1)]
-for i in range(1, n+1):
-    for j in s:
+n = int(input()) + 1
+dp = [i for i in range(n)]
+for i in range(1, n):
+    for j in range(1, n):
         if j*j > i: break
         if dp[i] > dp[i-j*j] + 1: dp[i] = dp[i-j*j] + 1
 print(dp[-1])
