562317f6b99f3cc70e53bcd3fb417ab641757fc8
Kir93 <kir931028@gmail.com>
2022-09-05 15:02:27 +0900

    update 2609 success

--- others/2609.py
+++ others/2609.py
@@ -1,27 +1,9 @@
-# 유클리드 호제법으로 다시 풀기
-
+# from math import gcd, lcm
+def gcd(a, b):
+    if b == 0: return a
+    else: return gcd(b, a%b)
+def lcm(a, b):
+    return a*b//gcd(a,b)
 x, y = map(int, input().split())
-big = 0
-small = max(x,y)
-xc = yc = 1
-if x>y: flag = x
-else: flag = y
-for i in range(1, max(x+1, y+1)):
-    if x%i == 0 and y%i == 0: big = i
-while True:
-    if flag == x:
-        cf = y
-        cc = yc
-    else:
-        cf = x
-        cc = xc
-    if small == cf*cc:
-        break
-    elif small < cf*cc:
-        small = cf*cc
-        if cf == x: flag = x
-        else: flag = y
-    if cf == x: xc += 1
-    else: yc += 1
-print(big)
-print(small)
+print(gcd(x, y))
+print(lcm(x, y))
