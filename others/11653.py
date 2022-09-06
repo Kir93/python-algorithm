44121334c68d12f69951acd43acc0b84b3db34fd
Kir93 <kir931028@gmail.com>
2022-09-07 00:00:43 +0900

    update 11653 time optimized

--- others/11653.py
+++ others/11653.py
@@ -1,7 +1,8 @@
-n = int(input())
+from sys import stdin, stdout
+n = int(stdin.readline())
 i = 2
 while n != 1:
     if n%i == 0:
-        print(i)
+        stdout.write(str(i)+'\n')
         n = n//i
     else: i += 1
