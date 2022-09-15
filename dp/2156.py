be34655fdae8ee60e684f5b3bbd4a4f13bbb56f0
Kir93 <kir931028@gmail.com>
2022-09-15 19:01:05 +0900

    update 2156

--- dp/2156.py
+++ dp/2156.py
@@ -1,8 +1,9 @@
-n = int(input())+1
-w = [0] * n
-dp = [0] * n
-for i in range(1,n):
-    w[i] = int(input())
-    if i == 1 or i == 2: dp[i] = w[i] + w[i-1]
-    else: dp[i] = max(w[i]+w[i-1]+dp[i-3], dp[i-1], dp[i-2]+w[i])
+w = [int(input()) for _ in range(int(input()))]
+dp = [0] * len(w)
+if(len(w)==1):
+    print(w[0])
+    exit()
+dp[0], dp[1] = w[0], w[0]+w[1]
+for i in range(2, len(w)):
+    dp[i] = max(w[i] + dp[i-2], w[i] + w[i-1] + dp[i-3], dp[i-1])
 print(max(dp))
