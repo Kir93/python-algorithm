322c7fe4b6d46b13a120f8cac98c747fa8cbcfd9
Kir93 <kir931028@gmail.com>
2022-09-28 13:32:14 +0900

    update 1158 success

--- others/1158.py
+++ others/1158.py
@@ -1,1 +1,11 @@
-# deque, list 풀 수 있음
+from collections import deque
+ls = deque()
+n, k = map(int, input().split())
+for i in range(1, n+1): ls.append(i)
+r = []
+while ls:
+    if len(ls) != 1: 
+        ls.rotate(-k+1)
+        r.append(ls.popleft())
+    else: r.append(ls.pop())        
+print(str(r).replace('[', '<').replace(']', '>'))
