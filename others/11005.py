f61fec07aa3c0186933e9e45655b6d11e3e2295d
Kir93 <kir931028@gmail.com>
2022-10-08 11:20:19 +0900

    update 11005 success

--- others/11005.py
+++ others/11005.py
@@ -1,3 +1,9 @@
 # 진법 변환은 몫과 나누기라는 것을 인지하자
 # 반복문 등호 말고 0이 아닐 때로
 temp = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+n, b = map(int, input().split())
+r = ''
+while n > 0:
+    r += temp[n%b]
+    n //= b
+print(r[::-1])
