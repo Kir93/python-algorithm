2fd772d5e2bf1821d4100c63914d14f1c3e3238a
Kir93 <kir931028@gmail.com>
2022-10-11 19:43:36 +0900

    update 1929 success

--- others/1929.py
+++ others/1929.py
@@ -1,2 +1,11 @@
 # 제곱근을 이용하자 int(x**0.5) or sqrt함수 이용
 # 근사 제곱근 구한 뒤 그 값으로 계산하면 시간초과 안 나옴
+from math import sqrt
+def isPrime(x):
+    for i in range(2, int(sqrt(x))+1):
+        if x%i == 0: return False
+    return True
+m, n = map(int, input().split())
+for i in range(m, n+1):
+    if i == 1: continue
+    if isPrime(i): print(i)
