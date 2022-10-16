430da126aa407419f91c90a15afefca25e0670a3
Kir93 <kir931028@gmail.com>
2022-10-16 16:50:54 +0900

    update 2579 fail

--- dp/2579.py
+++ dp/2579.py
@@ -1,10 +1,2 @@
-ls = []
-for _ in range(int(input())):
-    ls.append(int(input()))
-if len(ls) != 0:
-    dp = [0] * len(ls)
-    for i in range(len(ls)):
-        if i == 0: dp[i] = ls[0]
-        elif i == 1: dp[i] = ls[0] + ls[1]
-        else: dp[i] = max(ls[i]+ls[i-1]+dp[i-3], ls[i]+dp[i-2])
-    print(dp[-1])
+# 마지막 계단을 무조건 밟아야 되는 조건을 상기하자
+# dp 조건은 2개
