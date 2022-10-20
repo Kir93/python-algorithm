5835322025ceb79b8eb5f81445e5720794b683c5
Kir93 <kir931028@gmail.com>
2022-10-20 19:04:04 +0900

    update 11652 success

--- others/11652.py
+++ others/11652.py
@@ -1,2 +1,8 @@
 # 마이너스가 올 수 있는 것을 생각하자
 # sorted lambda를 이용하면 된다.
+ls = {}
+for _ in range(int(input())):
+    x = int(input())
+    if x in ls: ls[x] += 1
+    else: ls[x] = 1
+print(sorted(ls.items(), key=lambda x: (-x[1], x[0]))[0][0])
