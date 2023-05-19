b3893f6086ef4e646b5af5f7eb22b1ecde6c8597
Kir93 <kir931028@gmail.com>
2023-05-19 16:24:18 +0900

    add 9094

---
+++ running/9094.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    r = 0
+    for i in range(1, n-1):
+        for j in range(i+1, n):
+            x = (i**2 + j**2 + m)%(i*j)
+            if x == 0: r += 1
+    print(r)
