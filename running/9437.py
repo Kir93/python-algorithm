a2897601b9c5e16690f31abff9f2be4cbdee3e79
Kir93 <kir931028@gmail.com>
2023-05-24 14:45:22 +0900

    add 9437

---
+++ running/9437.py
@@ -0,0 +1,8 @@
+while True:
+    t = list(map(int, input().split()))
+    if t[0] == 0: break
+    for i in range(t[0]//4):
+        ls = [2*i+1, 2*i+2, t[0]-2*i-1, t[0]-2*i]
+        if t[1] in ls:
+            ls.remove(t[1])
+            print(*ls)
