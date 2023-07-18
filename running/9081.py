df1ca1349fff4acd8aed18fb2135aebef2f4be26
Kir93 <kir931028@gmail.com>
2023-07-18 15:01:54 +0900

    update 9081

--- running/9081.py
+++ running/9081.py
@@ -3,9 +3,9 @@ for _ in range(int(input())):
     flag = False
     for i in range(len(perm)-1, 0, -1):
         if flag: break
-        if ord(perm[i-1]) < ord(perm[i]):
+        if perm[i-1] < perm[i]:
             for j in range(len(perm)-1, 0, -1):
-                if ord(perm[i-1]) < ord(perm[j]):
+                if perm[i-1] < perm[j]:
                     perm[i-1], perm[j] = perm[j], perm[i-1]
                     perm = perm[:i] + sorted(perm[i:])
                     flag = True
