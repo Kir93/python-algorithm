eca2c1d87ed8234dc94d2cbbc84bc54b80f4a11b
Kir93 <kir931028@gmail.com>
2023-09-08 11:37:33 +0900

    add 9584

--- running2(351~450)/9584.py
+++ running2(351~450)/9584.py
@@ -1,13 +1,10 @@
-c = list(input())
+c = input()
 ls = []
 for _ in range(int(input())):
-    k = list(input())
-    f = True
+    k = input()
     for i in range(len(c)):
-        if c[i] == '*': continue
-        elif c[i] != k[i]:
-            f = False
-            break
-    if f == True: ls.append(k)
+        if c[i] != '*':
+            if c[i] != k[i]: break
+    else: ls.append(k)
 print(len(ls))
-for x in ls: print(''.join(x))
+print('\n'.join(ls))
