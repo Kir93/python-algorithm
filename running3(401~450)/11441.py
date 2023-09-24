87e3a0529965ba569355f0ebc0af3783c36ebe3a
Kir93 <kir931028@gmail.com>
2023-09-24 11:34:40 +0900

    add 11441

--- running3(401~450)/11441.py
+++ running3(401~450)/11441.py
@@ -1,12 +1,12 @@
 from sys import stdin
 input = stdin.readline
 
-n = int(input())
+n, m = map(int, input().split())
 ls = list(map(int, input().split()))
 preSum = [0] * (n + 1)
 
 for i in range(n): preSum[i] = preSum[i-1] + ls[i]
 
-for _ in range(int(input())):
+for _ in range(m):
     i, j = map(int, input().split())
     print(preSum[j-1] - preSum[i-2])
