bed6b59a7a97d1caa2b62565aace4efd2ea17c58
Kir93 <kir931028@gmail.com>
2023-10-30 14:49:28 +0900

    add 1655

---
+++ shortTermGrowth/1655.py
@@ -0,0 +1,14 @@
+import heapq
+max_h, min_h = [], []
+for _ in range(int(input())):
+    num = int(input())
+    if len(max_h) == len(min_h): heapq.heappush(max_h, -num)
+    else: heapq.heappush(min_h, num)
+    if len(max_h) >= 1 and len(min_h) >= 1 and max_h[0] * -1 > min_h[0]:
+        max_value = heapq.heappop(max_h) * -1
+        min_value = heapq.heappop(min_h)
+
+        heapq.heappush(max_h, min_value * -1)
+        heapq.heappush(min_h, max_value)
+    
+    print(max_h[0] * -1)
