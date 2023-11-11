118c03349a379331d213a76916467c4a0af3fc4f
Kir93 <kir931028@gmail.com>
2023-11-11 09:48:31 +0900

    add 7579

--- shortTermGrowth/7579.py
+++ shortTermGrowth/7579.py
@@ -1,22 +1,30 @@
-N, M = map(int, input().split())
-A = [0] + list(map(int, input().split()))
-C = [0] + list(map(int, input().split()))
-dp = [[0 for _ in range(sum(C)+1)] for _ in range(N+1)]
-result = sum(C)
+import sys
+input = sys.stdin.readline
 
-for i in range(1, N+1):
-    byte = A[i]
-    cost = C[i]
-    
-    for j in range(1, sum(C) + 1):
-        if j < cost:
+n, m = map(int, input().rsplit())
+memories = list(map(int, input().rsplit()))
+cost = list(map(int, input().rsplit()))
+
+tc = sum(cost)
+result = sys.maxsize
+
+dp = [[0 for _ in range(tc+1)] for _ in range(n+1)]
+
+for i in range(n):
+    for j in range(tc):
+        if cost[i] > j:
             dp[i][j] = dp[i-1][j]
         else:
-            dp[i][j] = max(byte + dp[i-1][j-cost], dp[i-1][j])
-            
-        if dp[i][j] >= M:
+            dp[i][j] = max(dp[i-1][j], memories[i] + dp[i-1][j-cost[i]])
+
+        if dp[i][j] >= m:
             result = min(result, j)
-if M != 0:
-    print(result)
-else:
+
+if m==0:
     print(0)
+elif n==1:
+    print(cost[0])
+elif result == sys.maxsize:
+    print(n*m)
+else:
+    print(result)
