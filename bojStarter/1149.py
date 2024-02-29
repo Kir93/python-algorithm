f9bbf8cae24b90b7db0f29882202d1a0ccc421ec
Kir93 <kir931028@gmail.com>
2024-02-29 16:29:35 +0900

    add 1149

---
+++ bojStarter/1149.py
@@ -0,0 +1,12 @@
+n = int(input())
+a = [0]*n
+
+for i in range(n):
+    a[i] = list(map(int,input().split()))
+    
+for i in range(1,n):
+    a[i][0]= min(a[i-1][1],a[i-1][2]) + a[i][0]
+    a[i][1]= min(a[i-1][0],a[i-1][2]) + a[i][1]
+    a[i][2]= min(a[i-1][0],a[i-1][1]) + a[i][2]
+
+print(min(a[n-1][0],a[n-1][1],a[n-1][2]))
