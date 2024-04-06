21a74063d0aba7dc699bd3175be5bca90fe7838e
Kir93 <kir931028@gmail.com>
2024-04-06 11:29:44 +0900

    add  4485

---
+++ bojStarter2/4485.py
@@ -0,0 +1,44 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+INF = int(1e9)
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def dijkstra():
+    q = []
+    heapq.heappush(q, (graph[0][0], 0, 0))
+    distance[0][0] = 0
+
+    while q:
+        cost, x, y = heapq.heappop(q)
+
+        if x == n - 1 and y == n - 1:
+            print(f'Problem {count}: {distance[x][y]}')
+            break
+
+        for i in range(4):
+            new_x = x + dx[i]
+            new_y = y + dy[i]
+
+            if 0 <= new_x < n and 0 <= new_y < n:
+                new_cost = cost + graph[new_x][new_y]
+
+                if new_cost < distance[new_x][new_y]:
+                    distance[new_x][new_y] = new_cost
+                    heapq.heappush(q, (new_cost, new_x, new_y))
+
+count = 1
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+
+    graph = [list(map(int, input().split())) for _ in range(n)]
+    distance = [[INF] * n for _ in range(n)]
+
+    dijkstra()
+    count += 1
