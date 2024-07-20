47fbfe90f1666d2564fcbe9db517b6af1aa34e1c
Kir93 <kir931028@gmail.com>
2024-07-21 08:02:06 +0900

    new file:   beginner/2846.py

---
+++ beginner/2846.py
@@ -0,0 +1,13 @@
+n = int(input())
+li = list(map(int, input().split()))
+a = 0
+re = []
+
+for i in range(n-1):
+    if li[i] < li[i+1]: a += li[i+1] - li[i]
+    else:
+        re.append(a)
+        a = 0
+        
+re.append(a)
+print(max(re))
