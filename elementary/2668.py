6cc8847fa855f997678ee45bdcdbd606cf1e7e9d
Kir93 <kir931028@gmail.com>
2024-08-17 12:18:00 +0900

    new file:   elementary/2668.py

---
+++ elementary/2668.py
@@ -0,0 +1,23 @@
+import sys
+input=sys.stdin.readline
+
+def dfs(v,i):
+    visited[v]=True
+    w=data[v]
+    if not visited[w]:
+        dfs(w,i)
+    elif visited[w] and w==i:
+        result.append(w)
+        
+n=int(input())
+data=[0]+[int(input()) for _ in range(n)]
+result=[]
+
+for i in range(1,n+1):
+    visited=[False]*(n+1)
+    dfs(i,i)
+
+print(len(result))
+result.sort()
+
+for i in result: print(i)
