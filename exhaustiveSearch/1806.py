ea91b870d5c252074501b38d979dc06834852d43
Kir93 <kir931028@gmail.com>
2024-11-05 10:32:25 +0900

    modified:   exhaustiveSearch/1806.py

--- exhaustiveSearch/1806.py
+++ exhaustiveSearch/1806.py
@@ -6,14 +6,17 @@ sum = 0
 min_length=1e9
 
 while True:
-    if right == n: break
     if sum >= s:
         min_length=min(min_length,right-left)
         sum -= nums[left]
         left +=1
+    elif right == n:
+        break
     else:
         sum += nums[right]
         right+=1
 
-if min_length == 1e9: print(0)
-else: print(min_length)
+if min_length == 1e9:
+    print(0)
+else:
+    print(min_length)
