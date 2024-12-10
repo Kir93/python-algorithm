9a309d412b07104a3ee36db0412bff5c3fda74ab
Kir93 <kir931028@gmail.com>
2024-12-10 10:22:59 +0900

    add 12789

---
+++ bojStarter/12789.py
@@ -0,0 +1,14 @@
+N = int(input())
+students = list(map(int, input().split())) 
+stack = []
+now_turn = 1
+
+for student in students:
+    stack.append(student)
+
+    while stack and stack[-1] == now_turn:
+        stack.pop()
+        now_turn +=1
+
+if stack: print('Sad') 
+else: print('Nice')
