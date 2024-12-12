77ea4677705e37a9b4dbfadc227a234de5e853c3
Kir93 <kir931028@gmail.com>
2024-12-12 10:15:35 +0900

    new file:   bojStarter/18258.py

---
+++ bojStarter/18258.py
@@ -0,0 +1,24 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n = int(input())
+queue = deque([])
+
+for i in range(n):
+    com = input().split()
+
+    if com[0] == 'push': queue.append(com[1])
+    elif com[0] == 'pop':
+        if len(queue) == 0: print(-1)
+        else: print(queue.popleft())
+    elif com[0] == 'size': print(len(queue))
+    elif com[0] == 'empty':
+        if len(queue) == 0: print(1)
+        else: print(0)
+    elif com[0] == 'front':
+        if len(queue) == 0: print(-1)
+        else: print(queue[0])
+    elif com[0] == 'back':
+        if len(queue) == 0: print(-1)
+        else: print(queue[-1])
