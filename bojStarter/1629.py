f615706aa2ea94e82b6ba898d3b3010642eae506
Kir93 <kir931028@gmail.com>
2025-01-10 22:59:20 +0900

    new file:   bojStarter/1629.py

---
+++ bojStarter/1629.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+a,b,c = map(int,input().split())
+
+def multi (a,n):
+  if n == 1:
+      return a%c
+  else:
+      tmp = multi(a,n//2)
+      if n %2 ==0:
+          return (tmp * tmp) % c
+      else:
+          return (tmp  * tmp *a) %c
+          
+print(multi(a,b))
