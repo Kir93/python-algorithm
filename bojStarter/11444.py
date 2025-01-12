5184f999db78434cec6fea987096162a2c8244b9
Kir93 <kir931028@gmail.com>
2025-01-12 12:26:41 +0900

    new file:   bojStarter/11444.py

---
+++ bojStarter/11444.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+p = 1000000007
+
+def mul(A, B):
+    n = len(A)
+    Z = [[0]*n for _ in range(n)]
+    
+    for row in range(n):
+        for col in range(n):
+            e = 0
+            for i in range(n):
+                e += A[row][i] * B[i][col]
+            Z[row][col] = e % p
+            
+    return Z
+
+def square(A, k):
+    if k == 1:
+        for x in range(len(A)):
+            for y in range(len(A)):
+                A[x][y] %= p
+        return A
+    
+    tmp = square(A, k//2)
+    if k % 2:
+        return mul(mul(tmp, tmp), A)
+    else:
+        return mul(tmp, tmp)
+    
+fib_matrix = [[1, 1], [1, 0]]
+print(square(fib_matrix, n)[0][1])
