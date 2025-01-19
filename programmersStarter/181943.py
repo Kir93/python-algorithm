648ea06d49b42d8aca18d2f78aa690b768f6fc79
Kir93 <kir931028@gmail.com>
2025-01-19 12:53:48 +0900

    remove test code

--- programmersStarter/181943.py
+++ programmersStarter/181943.py
@@ -2,6 +2,3 @@
 def solution(my_string, overwrite_string, s):
     answer = my_string[:s] + overwrite_string + my_string[s + len(overwrite_string):]
     return answer
-
-solution("He11oWor1d","lloWorl", 2)
-# solution("Program29b8UYP","merS123", 7)
