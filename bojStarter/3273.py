acb37129d9c0505573c295982d6d91030ab4d119
Kir93 <kir931028@gmail.com>
2025-01-28 20:52:37 +0900

    new file:   bojStarter/3273.py

---
+++ bojStarter/3273.py
@@ -0,0 +1,19 @@
+import sys
+
+n = int(input())
+numbers = sorted(list(map(int, sys.stdin.readline().split())))
+x = int(input())
+
+answer = 0
+left, right = 0, n-1
+
+while left < right:
+    temp = numbers[left] + numbers[right]
+    if temp == x:
+        answer += 1
+        left += 1
+    elif temp < x:
+        left += 1
+    else:
+        right -= 1
+print(answer)
