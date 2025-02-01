273b6e99f1f1d3a7108f43dff9dd6fc092abcc25
Kir93 <kir931028@gmail.com>
2025-02-01 18:16:28 +0900

    retry 120808

--- programmersBeginner/120808.py
+++ programmersBeginner/120808.py
@@ -1,4 +1,4 @@
-# 분수의 덧셈 - retry
+# 분수의 덧셈
 import math
 def solution(numer1, denom1, numer2, denom2):
     numer = numer1 * denom2 + numer2 * denom1
@@ -11,3 +11,13 @@ from fractions import Fraction
 def solution(numer1, denom1, numer2, denom2):
     answer = Fraction(numer1, denom1) + Fraction(numer2, denom2)
     return [answer.numerator, answer.denominator]
+
+# retry
+def gcd(a, b):
+    return a if b == 0 else gcd(b, a%b)
+
+def solution(numer1, denom1, numer2, denom2):
+    numer = (numer1 * denom2) + (numer2 * denom1)
+    denom = denom1 * denom2
+    GCD = gcd(numer, denom)
+    return [numer//GCD, denom//GCD]
