dfad42ee050548d04914d111450df4522d0984a4
Kir93 <kir931028@gmail.com>
2025-02-01 18:36:54 +0900

    retry 181832

--- programmersStarter/181832.py
+++ programmersStarter/181832.py
@@ -1,4 +1,4 @@
-# 정수를 나선형으로 배치하기 - retry
+# 정수를 나선형으로 배치하기
 def solution(n):
     if n == 1: return [[1]]
 
@@ -21,4 +21,28 @@ def solution(n):
             y -= 1
             if y == 1 or answer[y-1][x] != 0: dir = 'r'
             
+    return answer
+
+# retry
+def solution(n):
+    if n == 1: return [[1]]
+
+    answer = [[0] * n for _ in range(n)]
+    mode = 'r'
+    y = x = 0
+    
+    for i in range(n**2):
+        answer[y][x] = i + 1
+        if mode == 'r':
+            x += 1
+            if x == n-1 or answer[y][x+1] != 0: mode = 'b'
+        elif mode == 'b':
+            y += 1
+            if y == n-1 or answer[y+1][x] != 0: mode = 'l'
+        elif mode == 'l':
+            x -= 1
+            if x == 0 or answer[y][x-1] != 0: mode = 't'
+        elif mode == 't':
+            y -= 1
+            if y == 1 or answer[y-1][x] != 0: mode = 'r'
     return answer
