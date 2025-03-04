4aedba3839b0d52e835093da31f8c21c62358dbd
Kir93 <kir931028@gmail.com>
2025-03-04 22:37:19 +0900

    retry fail

--- programmersBeginner/120866.py
+++ programmersBeginner/120866.py
@@ -7,17 +7,21 @@ def solution(board):
     
     boom = []
     
+    # 폭탄 위치 저장
     for i in range(n):
         for j in range(n):
             if board[i][j] == 1: boom.append((i, j))
     
+    # 폭탄 주변 8방향에 1로 표시
     for x, y in boom:
         for i in range(8):
             nx = x + dx[i]
             ny = y + dy[i]
+            # 주변의 범위를 벗어나지 않았을 때 
             if 0 <= nx < n and 0 <= ny < n:
                 board[nx][ny] = 1
     
+    # 안전지대 개수 세기
     for i in range(n):
         for j in range(n):
             if board[i][j] == 0: answer += 1
