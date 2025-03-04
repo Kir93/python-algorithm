d93b9bb7cee34a3f310bbd8283e13542da8a338b
Kir93 <kir931028@gmail.com>
2025-03-04 22:30:07 +0900

    retry 120836

--- programmersBeginner/120836.py
+++ programmersBeginner/120836.py
@@ -1,4 +1,4 @@
-# 순서쌍의 개수 - retry
+# 순서쌍의 개수
 def solution(n):
     answer = 0
     for i in range(1, n+1):
@@ -18,6 +18,16 @@ def solution(n):
             if i * i == n:
                 answer -= 1
 
+    return answer
+
+# retry
+def solution(n):
+    answer = 0
+    for i in range(1, int(n**0.5)+1):
+        if n%i == 0:
+            answer += 2
+        if i * i == n:
+            answer -= 1
     return answer
 
 # 순서 쌍의 경우 0.5제곱을 int로 줄인 뒤 + 1더해 순서대로 확인하며
