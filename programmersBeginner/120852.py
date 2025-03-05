575796b49158193509a6e6c964a119cefa496ab2
Kir93 <kir931028@gmail.com>
2025-03-05 23:33:32 +0900

    retry success

--- programmersBeginner/120852.py
+++ programmersBeginner/120852.py
@@ -1,4 +1,4 @@
-# 소인수 분해 - retry
+# 소인수분해
 def primeFactors(n):
     arr, d = [], 2
     while n > 1:
@@ -10,3 +10,13 @@ def primeFactors(n):
     
 def solution(n):
     return primeFactors(n)
+
+# retry
+def solution(n):
+    answer, d = [], 2
+    while n > 1:
+        if n%d == 0:
+            answer.append(d)
+            n //= d
+        else: d += 1
+    return sorted(set(answer))
