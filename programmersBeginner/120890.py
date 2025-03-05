d55ee68fea9cc7d1d3477fcd1c7f2ad0ade519ac
Kir93 <kir931028@gmail.com>
2025-03-05 23:26:51 +0900

    retry success

--- programmersBeginner/120890.py
+++ programmersBeginner/120890.py
@@ -1,7 +1,15 @@
-# 가까운 수 - retry
+# 가까운 수
 def solution(array, n):
     box = []
     array.sort()
     for x in array:
         box.append(abs(n-x))
     return array[box.index(min(box))]
+
+# retry
+def solution(array, n):
+    array.sort()
+    answer = []
+    for i in array:
+        answer.append(abs(n-i))
+    return array[answer.index(min(answer))]
