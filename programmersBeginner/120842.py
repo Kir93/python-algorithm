671a8e2781dba9c6adb5d15495a57841fbc73090
Kir93 <kir931028@gmail.com>
2025-03-06 23:39:42 +0900

    retry success

--- programmersBeginner/120842.py
+++ programmersBeginner/120842.py
@@ -1,3 +1,7 @@
-# 2차원으로 만들기 - retry
+# 2차원으로 만들기
 def solution(num_list, n):
     return [num_list[i:i+n] for i in range(0, len(num_list), n)]
+
+# retry
+def solution(num_list, n):
+    return [num_list[i:n + i] for i in range(0, len(num_list), n)]
