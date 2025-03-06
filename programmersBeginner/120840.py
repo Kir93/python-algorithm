cc3936765209cdaa48192280026d0a7dfb0be511
Kir93 <kir931028@gmail.com>
2025-03-06 23:44:47 +0900

    retry success

--- programmersBeginner/120840.py
+++ programmersBeginner/120840.py
@@ -1,4 +1,4 @@
-# 구슬을 나누는 경우의 수 - retry
+# 구슬을 나누는 경우의 수
 def fac(num):
     r = 1
     for i in range(1, num+1): r *= i
@@ -24,3 +24,28 @@ def solution(balls, share):
 from math import comb
 def solution(balls, share):
     return comb(balls, share)
+
+# retry 1
+import math
+def solution(balls, share):
+    n = math.factorial(balls)
+    m = math.factorial(share)
+    nm = math.factorial(balls-share)
+    return n / (nm * m)
+
+# retry 2
+import math
+def solution(balls, share):
+    return math.comb(balls, share)
+
+# retry 3
+def fac(num):
+    r = 1
+    for i in range(1, num+1): r *= i
+    return r
+
+def solution(balls, share):
+    n = fac(balls)
+    m = fac(share)
+    nm = fac(balls - share)
+    return n / (m * nm)
