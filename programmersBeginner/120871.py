e62e1e2ca982eff196fa6d1ff256d6f25703b4a2
Kir93 <kir931028@gmail.com>
2025-03-17 23:43:28 +0900

    retry success

--- programmersBeginner/120871.py
+++ programmersBeginner/120871.py
@@ -1,4 +1,4 @@
-# 저주의 숫자 3 - retry
+# 저주의 숫자 3
 def solution(n):
     answer = 0
     
@@ -8,3 +8,13 @@ def solution(n):
         answer += 1
 
     return answer - 1
+
+# retry
+def solution(n):
+    answer = 0
+    for _ in range(n):
+        answer += 1
+        while answer%3 == 0 or '3' in str(answer):
+            answer += 1
+            
+    return answer
