5c0f797069ecceb97f18768432bf540bbab684dd
Kir93 <kir931028@gmail.com>
2025-03-23 17:44:50 +0900

    retry success

--- programmersBeginner/120956.py
+++ programmersBeginner/120956.py
@@ -1,4 +1,4 @@
-# 옹알이(1) - retry
+# 옹알이(1)
 def solution(babbling):
     babyWord = ["aya", "ye", "woo", "ma"]
     answer = 0
@@ -7,4 +7,16 @@ def solution(babbling):
             if w in x: x = x.replace(w, '*')
             
         if len(x) == x.count('*'): answer += 1
+    return answer
+
+# retry
+def solution(babbling):
+    word = ["aya", "ye", "woo", "ma"]
+    answer = 0
+    
+    for b in babbling:
+        for w in word:
+            if w in b: b = b.replace(w, '*')
+        if len(b) == b.count('*'): answer += 1
+        
     return answer
