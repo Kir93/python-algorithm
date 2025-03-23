e4749e63ae0c091cac4143bd82df654b46e1f332
Kir93 <kir931028@gmail.com>
2025-03-23 17:20:33 +0900

    retry success

--- programmersBeginner/120878.py
+++ programmersBeginner/120878.py
@@ -1,4 +1,4 @@
-# 유한소수 판별하기 - retry
+# 유한소수 판별하기
 
 # 유클리드 호제법을 이용한 최대공약수 구하기
 def gcd(a, b):
@@ -14,4 +14,19 @@ def solution(a, b):
         elif n%5 == 0:
             n //= 5
         else: return 2
+    return 1
+
+# retry
+from math import gcd
+
+def solution(a, b):
+    n = b // gcd(a, b)
+    
+    while n > 1:
+        if n % 2 == 0:
+            n //= 2
+        elif n % 5 == 0:
+            n //= 5
+        else: return 2
+
     return 1
