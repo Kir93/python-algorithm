a7fdcb6ada514e61bc6048fd06cf8155c0278eb4
Kir93 <kir931028@gmail.com>
2025-03-23 17:55:09 +0900

    retry success

--- programmersBeginner/120866.py
+++ programmersBeginner/120866.py
@@ -1,4 +1,4 @@
-# 안전지대 - retry
+# 안전지대
 def solution(board):
     answer = 0
     n = len(board)
@@ -22,6 +22,32 @@ def solution(board):
                 board[nx][ny] = 1
     
     # 안전지대 개수 세기
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] == 0: answer += 1
+    
+    return answer
+
+# retry
+def solution(board):
+    answer = 0
+    
+    dx = [-1, 1, -1, 0, 1, -1, 0, 1]
+    dy = [0, 0, 1, 1, 1, -1, -1, -1]
+    n = len(board)
+    boom = []
+    
+    for x in range(n):
+        for y in range(n):
+            if board[x][y] == 1: boom.append([x, y])
+            
+    for x, y in boom:
+        for i in range(8):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n:
+                board[nx][ny] = 1
+                
     for i in range(n):
         for j in range(n):
             if board[i][j] == 0: answer += 1
