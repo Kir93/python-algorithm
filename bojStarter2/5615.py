ce9626ea9b8b5f13a62c0414a4dbbd9dd3fb7d57
Kir93 <kir931028@gmail.com>
2025-04-21 23:57:33 +0900

    modified:   bojStarter2/5615.py

--- bojStarter2/5615.py
+++ bojStarter2/5615.py
@@ -1,39 +1,45 @@
+import sys
+input = sys.stdin.readline
+
 def powmod(a,b,m):
     result = 1
-    while b > 0:
-        if b % 2 != 0:
-            result = (result * a) % m
-        b //= 2
-        a = (a * a) % m
-
+    while b>0:
+        if b%2 != 0:
+            result = (result*a)%m
+        b//=2
+        a = (a*a)%m
     return result
 
-def mr(n,a):
+
+def MillerRabin(n,a):
     r = 0
     d = n-1
-    while (d%2 == 0):
+    while(d%2==0):
         r += 1
-        d = d // 2
+        d //=2
     x = powmod(a,d,n)
-    if x == 1 or x == n-1:
+    if x==1 or x==n-1:
         return True
     for i in range(0,r-1):
         x = powmod(x,2,n)
-        if x == n-1:
+        if x==n-1:
             return True
     return False
 
-import sys
-ans = 0
-m = int(sys.stdin.readline())
-for i in range(m):
-    k = int(sys.stdin.readline())
-    check = 0
-    for i in [2, 3,5,7,11]:
-        if mr(2*k+1,i) == False:
-            break
-        else:
-            check += 1
-    if check == 5:
-        ans += 1
-print(ans)
+if __name__=='__main__':
+    ans = 0
+    n = int(input())
+    for _ in range(n):
+        k = int(input())
+        cnt = 0
+        if k<4:
+            ans+=1
+            continue
+        for i in [2,3,5,7,11]:
+            if MillerRabin(2*k+1,i)==False:
+                break
+            else:
+                cnt+= 1
+        if cnt==5:
+            ans+=1
+    print(ans)
