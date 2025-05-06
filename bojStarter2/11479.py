e9b773a8620a4497da8b404e10cf381495e11683
Kir93 <kir931028@gmail.com>
2025-05-06 20:24:44 +0900

    new file:   bojStarter2/11479.py

---
+++ bojStarter2/11479.py
@@ -0,0 +1,84 @@
+import sys
+input=sys.stdin.readline
+
+def suffix_array(A):
+    n = len(A)
+    buckets = [0] * (max(A) + 2)
+    for a in A:
+        buckets[a + 1] += 1
+    for b in range(1, len(buckets)):
+        buckets[b] += buckets[b - 1]
+    isL = [1] * n
+    for i in reversed(range(n - 1)):
+        isL[i] = +(A[i] > A[i + 1]) if A[i] != A[i + 1] else isL[i + 1]
+
+    def induced_sort(LMS):
+        SA = [-1] * (n)
+        SA.append(n)
+        endpoint = buckets[1:]
+        for j in reversed(LMS):
+            endpoint[A[j]] -= 1
+            SA[endpoint[A[j]]] = j
+        startpoint = buckets[:-1]
+        for i in range(-1, n):
+            j = SA[i] - 1
+            if j >= 0 and isL[j]:
+                SA[startpoint[A[j]]] = j
+                startpoint[A[j]] += 1
+        SA.pop()
+        endpoint = buckets[1:]
+        for i in reversed(range(n)):
+            j = SA[i] - 1
+            if j >= 0 and not isL[j]:
+                endpoint[A[j]] -= 1
+                SA[endpoint[A[j]]] = j
+        return SA
+
+    isLMS = [+(i and isL[i - 1] and not isL[i]) for i in range(n)]
+    isLMS.append(1)
+    LMS = [i for i in range(n) if isLMS[i]]
+    if len(LMS) > 1:
+        SA = induced_sort(LMS)
+        LMS2 = [i for i in SA if isLMS[i]]
+        prev = -1
+        j = 0
+        for i in LMS2:
+            i1 = prev
+            i2 = i
+            while prev >= 0 and A[i1] == A[i2]:
+                i1 += 1
+                i2 += 1
+                if isLMS[i1] or isLMS[i2]:
+                    j -= isLMS[i1] and isLMS[i2]
+                    break
+            j += 1
+            prev = i
+            SA[i] = j
+        LMS = [LMS[i] for i in suffix_array([SA[i] for i in LMS])]
+    return induced_sort(LMS)
+
+
+def lcp_array(A, SA):
+    n = len(A)
+    rank = [0] * n
+    for i in range(n):
+        rank[SA[i]] = i
+    LCP = [0] * (n - 1)
+    k = 0
+    for i in range(n):
+        SAind = rank[i]
+        if SAind == n - 1:
+            continue
+        j = SA[SAind + 1]
+        while i + k < n and A[i + k] == A[j + k]:
+            k += 1
+        LCP[SAind] = k
+        k -= k > 0
+    return LCP
+
+s=list(input().rstrip())
+n=len(s)
+line=[ord(c) for c in s]
+sa=suffix_array(line)
+lcp=lcp_array(line,sa)
+print(n*(n+1)//2-sum(lcp))
