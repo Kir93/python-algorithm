22ef33624c8f6e5af9ee15ab338ff62244ec47ea
CR Han <kir931028@gmail.com>
2025-05-09 22:01:33 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,6 +1,2 @@
 def solution(absolutes, signs):
-    answer = 0
-    for i in range(len(signs)):
-        if signs[i] == True: answer += absolutes[i]
-        else: answer += -absolutes[i]
-    return answer
+    return sum(absolute if sign else -absolute for absolute, sign in zip(absolutes, signs))
