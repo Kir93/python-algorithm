735e056945c7d1e2ad56ecf7ccf9a44d72d83de8
CR Han <kir931028@gmail.com>
2025-05-23 22:35:06 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,2 +1,2 @@
 -- 코드를 작성해주세요
-select id, email, first_name, last_name from DEVELOPER_INFOS where skill_1 = 'Python' or skill_2 = 'Python' or skill_3 = 'Python' order by id
+select id, email, first_name, last_name from DEVELOPER_INFOS where 'Python' in (skill_1, skill_2, skill_3) order by id
