f9c6dc46faf5befba19e5bcd73f306e5092d7d61
CR Han <kir931028@gmail.com>
2025-05-31 18:29:36 +0900

    [Bronze II] Title: 이상한 곱셈, Time: 92 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.py
+++ 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.py
@@ -1,8 +1,12 @@
 a, b = map(str, input().split())
-r = 0
 
-for x in a:
-    for y in b:
-        r += int(x) * int(y)
+sumA = sum([int(i) for i in a])
+sumB = sum([int(i) for i in b])
 
-print(r)
+print(sumA * sumB)
+
+# r = a₁×b₁ + a₁×b₂ + ... + a₁×bₘ + a₂×b₁ + a₂×b₂ + ... + a₂×bₘ + ... + aₙ×b₁ + aₙ×b₂ + ... + aₙ×bₘ
+# 이걸 인수분해하면
+# r = a₁×(b₁+b₂+...+bₘ) + a₂×(b₁+b₂+...+bₘ) + ... + aₙ×(b₁+b₂+...+bₘ)
+#   = (a₁+a₂+...+aₙ) × (b₁+b₂+...+bₘ)
+#   = sum(a) × sum(b)
