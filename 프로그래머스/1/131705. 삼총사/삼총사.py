57680924cfc5b4bf8f651a17cf347390a162bc90
CR Han <kir931028@gmail.com>
2025-06-05 12:37:45 +0900

    [level 1] Title: 삼총사, Time: 0.07 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,11 +1,10 @@
+from itertools import combinations
+
 def solution(number):
     answer = 0
-    l = len(number)
     
-    for i in range(l - 2):
-        for j in range(i+1, l - 1):
-            for k in range(j+1, l):
-                if number[i] + number[j] + number[k] == 0:
-                    answer += 1
-                    
+    for combi in combinations(number, 3):
+        if sum(combi) == 0:
+            answer += 1
+    
     return answer
