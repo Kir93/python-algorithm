b4fde4fc4b322102b321e561000a185e1adc65dd
CR Han <kir931028@gmail.com>
2025-06-17 23:51:30 +0900

    [level 1] Title: 과일 장수, Time: 84.70 ms, Memory: 20.8 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -2,9 +2,7 @@ def solution(k, m, score):
     score.sort(reverse=True)
     answer = 0
     
-    for i in range(0, len(score), m):
-        box = score[i:m + i]
-        if len(box) < m: break
-        answer += min(box) * m
+    for i in range(m - 1, len(score), m):
+        answer += score[i] * m
         
     return answer
