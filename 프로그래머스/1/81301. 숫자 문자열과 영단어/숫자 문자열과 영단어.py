95c515172e3f69e9f7c244c18bbf02d41d184573
CR Han <kir931028@gmail.com>
2025-06-21 20:07:55 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,17 +1,7 @@
 numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
 def solution(s):
-    answer = []
-    convert_num = []
+    for i, num in enumerate(numbers):
+        s = s.replace(num, str(i))
 
-    for num in s:
-        if num.isdigit():
-            answer.append(num)
-        else:
-            if numbers.count(str(''.join(convert_num))+num) > 0:
-                answer.append(str(numbers.index(''.join(convert_num)+num)))
-                convert_num = []
-            else:
-                convert_num.append(num)
-
-    return int(''.join(answer))
+    return int(s)
