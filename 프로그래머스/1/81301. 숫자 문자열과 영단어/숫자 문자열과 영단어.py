c440c0ce0eef0737926e93dca276130e53ef47f7
CR Han <kir931028@gmail.com>
2025-06-21 20:19:07 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.04 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -2,6 +2,22 @@ numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight
 num_dict = {word: str(i) for i, word in enumerate(numbers)}
 
 def solution(s):
-    for word, digit in num_dict.items():
-        s = s.replace(word, digit)
-    return int(s)
+    answer = []
+    convert_str = []
+
+    for word in s:
+        if word.isdigit():
+            answer.append(word)
+        else:
+            convert_str.append(word)
+            join_str = ''.join(convert_str)
+            if join_str in num_dict:
+                answer.append(num_dict[join_str])
+                convert_str = []
+
+    return int(''.join(answer))
+
+# def solution(s):
+#     for word, digit in num_dict.items():
+#         s = s.replace(word, digit)
+#     return int(s)
