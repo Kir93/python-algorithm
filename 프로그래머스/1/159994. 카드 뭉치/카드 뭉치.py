b3ec80bd2ab8343fc79aa5da94f91583e9804b19
CR Han <kir931028@gmail.com>
2025-06-23 23:25:18 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,9 @@
+def solution(cards1, cards2, goal):
+    for card in goal:
+        if len(cards1) and cards1[0] == card:
+            cards1.pop(0)
+        elif len(cards2) and cards2[0] == card:
+            cards2.pop(0)
+        else:
+            return 'No'
+    return 'Yes'
