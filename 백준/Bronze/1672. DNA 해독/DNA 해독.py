07b15facafd7c17bbf728525263e232dc194a97c
CR Han <kir931028@gmail.com>
2025-06-23 23:15:40 +0900

    [Bronze I] Title: DNA 해독, Time: 140 ms, Memory: 113276 KB -BaekjoonHub

--- 백준/Bronze/1672. DNA 해독/DNA 해독.py
+++ 백준/Bronze/1672. DNA 해독/DNA 해독.py
@@ -1,14 +1,13 @@
-import sys
-
-input = sys.stdin.readline
-dna_map = {'AA':'A', 'AG': 'C',  'AC':'A', 'AT':'G', 'GA':'C', 'GG':'G', 'GC':'T', 'GT':'A', 'CA':'A', 'CG':'T', 'CC':'C', 'CT':'G', 'TA':'G', 'TG':'A', 'TC':'G', 'TT':'T'}
+dna_map = {'AA':'A', 'AG': 'C', 'AC':'A', 'AT':'G', 'GA':'C', 'GG':'G', 'GC':'T', 'GT':'A', 'CA':'A', 'CG':'T', 'CC':'C', 'CT':'G', 'TA':'G', 'TG':'A', 'TC':'G', 'TT':'T'}
 
 n = int(input())
-dna = list(input().strip())
+dna = input()
 
-while len(dna) > 1:
-    check_dna = dna[-2] + dna[-1]
-    dna.pop()
-    dna[-1] = dna_map[check_dna]
+i = len(dna) - 2
+r = dna[-1]
 
-print(dna[0])
+while i >= 0:
+    r = dna_map[dna[i] + r]
+    i -= 1
+
+print(r)
