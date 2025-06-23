a657ffff90f433ab04f45cbc7e478b4c018f4b7c
CR Han <kir931028@gmail.com>
2025-06-23 23:27:33 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,9 +1,10 @@
 def solution(cards1, cards2, goal):
+    cards1_index = cards2_index = 0
     for card in goal:
-        if len(cards1) and cards1[0] == card:
-            cards1.pop(0)
-        elif len(cards2) and cards2[0] == card:
-            cards2.pop(0)
+        if cards1_index < len(cards1) and cards1[cards1_index] == card:
+            cards1_index += 1
+        elif cards2_index < len(cards2) and cards2[cards2_index] == card:
+            cards2_index += 1
         else:
             return 'No'
     return 'Yes'
