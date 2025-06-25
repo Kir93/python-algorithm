001999965985d56ab61fc9356b9b332a94025548
CR Han <kir931028@gmail.com>
2025-06-25 22:43:31 +0900

    [level 1] Title: 소수 만들기, Time: 17.79 ms, Memory: 9.24 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.py
@@ -0,0 +1,20 @@
+from itertools import combinations
+
+def is_prime(num):
+    if num < 2:
+        return False
+    
+    for i in range(2, int(num**0.5)+1):
+        if num%i == 0:
+            return False
+        
+    return True
+
+def solution(nums):
+    answer = 0
+    
+    for comb in combinations(nums, 3):
+        if is_prime(sum(comb)):
+            answer += 1
+        
+    return answer
