26e184e1fb26ca9a104d9aab04048ddff3edabee
CR Han <kir931028@gmail.com>
2025-07-02 23:11:27 +0900

    [level 1] Title: 옹알이 (2), Time: 0.23 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,27 +1,23 @@
 def solution(babbling):
     words = ['aya', 'ye', 'woo', 'ma']
     answer = 0
-
-    for x in babbling:
-        idx = 0
+    
+    for b in babbling:
         prev = ''
-        is_valid = True
-
-        while idx < len(x):
-            matched = False
+        i = 0
+        
+        while i < len(b):
+            found = False
             
             for word in words:
-                if x.startswith(word, idx) and word != prev:
+                if b.startswith(word, i) and word != prev:
+                    i += len(word)
                     prev = word
-                    idx += len(word)
-                    matched = True
+                    found = True
                     break
-                    
-            if not matched:
-                is_valid = False
-                break
 
-        if is_valid:
-            answer += 1
+            if not found: break
 
+        if found: answer += 1
+            
     return answer
