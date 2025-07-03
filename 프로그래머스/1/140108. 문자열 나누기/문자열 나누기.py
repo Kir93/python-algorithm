3f82667f67bdac71b7d8b788d5a991ed97fceb28
CR Han <kir931028@gmail.com>
2025-07-03 17:13:15 +0900

    [level 1] Title: 문자열 나누기, Time: 1.67 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,21 +1,18 @@
 def solution(s):
-    answer = 0
+    answer = len_x = len_diff = 0
     prev = ''
-    x_count = 0
-    diff_count = 0
     
     for x in s:
-        if x_count == 0:
+        if len_x == 0:
             prev = x
             answer += 1
-
-        if prev == x:
-            x_count += 1
-        else:
-            diff_count += 1
         
-        if x_count == diff_count:
-            x_count = 0
-            diff_count = 0
-            
+        if x == prev:
+            len_x += 1
+        else:
+            len_diff += 1
+        
+        if len_x == len_diff:
+            len_x = len_diff = 0
+        
     return answer
