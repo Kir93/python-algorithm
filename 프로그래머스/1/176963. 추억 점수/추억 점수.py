20da9c4f97e7e6b71022e425afeebb46372893a8
CR Han <kir931028@gmail.com>
2025-07-16 17:40:31 +0900

    [level 1] Title: 추억 점수, Time: 1.46 ms, Memory: 9.66 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,12 +1,13 @@
 def solution(name, yearning, photo):
     answer = []
+    point_dict = dict(zip(name,yearning))
     
     for ph in photo:
         s = 0
         
         for p in ph:
-            if p in name:
-                s += yearning[name.index(p)]
+            if p in point_dict:
+                s += point_dict[p]
                 
         answer.append(s)
                 
