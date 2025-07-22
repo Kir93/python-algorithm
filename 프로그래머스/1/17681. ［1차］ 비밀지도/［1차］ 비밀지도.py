647bc442f8de4ce303c775fdae2aabdfdabf4ce3
CR Han <kir931028@gmail.com>
2025-07-22 16:11:34 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.07 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,16 +1,11 @@
 def solution(n, arr1, arr2):
     answer = []
+    
     for i in range(len(arr1)):
-        bin_arr1 = format(arr1[i], 'b').zfill(n)
-        bin_arr2 = format(arr2[i], 'b').zfill(n)
-        b = ''
-        
-        for j in range(n):
-            if bin_arr1[j] == '1' or bin_arr2[j] == '1':
-                b += '#'
-            else:
-                b += ' '
-        
-        answer.append(b)
+        combined = format(arr1[i] | arr2[i], f'0{n}b')
+        answer.append(''.join('#' if c == '1' else ' ' for c in combined))
         
     return answer
+
+# format(x, f'0{n}b')로 자릿수 원하는 방식으로 맞출 수 있음.
+# |로 2진수 변환 없이 bit or 연산 가능
