9ef413059a472618337ebabf4708f038065541d3
CR Han <kir931028@gmail.com>
2025-07-31 16:46:34 +0900

    [level 1] Title: 덧칠하기, Time: 7.88 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.py
@@ -1,14 +1,10 @@
 def solution(n, m, section):
     answer = 0
-    area = [0] * (n+1)
     i = 0
-
-    while i < len(section):
-        start = section[i]
-        answer += 1
-        painted_end = start + m - 1
-
-        while i < len(section) and section[i] <= painted_end:
-            i += 1
-
+    
+    for s in section:
+        if s > i:
+            answer += 1
+            i = s + m - 1
+            
     return answer
