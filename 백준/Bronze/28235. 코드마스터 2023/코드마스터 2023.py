f68ba3db576907ef6b5b0711b4b3fe4169a123bc
CR Han <kir931028@gmail.com>
2025-08-02 09:40:31 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 112 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,10 @@
+slogan = input()
+
+if slogan == 'SONGDO':
+    print('HIGHSCHOOL')
+elif slogan == 'CODE':
+    print('MASTER')
+elif slogan == '2023':
+    print('0611')
+else:
+    print('CONTEST')
