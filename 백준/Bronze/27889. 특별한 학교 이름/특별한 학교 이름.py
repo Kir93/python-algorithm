7a232e75d0a608de0d4cf340334dd9e642ecbe5b
CR Han <kir931028@gmail.com>
2025-08-06 14:05:37 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,8 @@
+GEC = {
+  'NLCS': 'North London Collegiate School',
+  'BHA': 'Branksome Hall Asia',
+  'KIS': 'Korea International School',
+  'SJA': 'St. Johnsbury Academy',
+}
+
+print(GEC[input()])
