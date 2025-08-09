faad140b6b0663103114795985827bad1b4f6e96
CR Han <kir931028@gmail.com>
2025-08-09 14:46:34 +0900

    [Bronze V] Title: 身長 (Height), Time: 92 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 'dev/stdin' : 'run/input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+console.log(parseInt(input[1], 10) - parseInt(input[0], 10));
