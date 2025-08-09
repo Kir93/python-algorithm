83c932b80475aba8f76874fa8ca7b72996697c25
CR Han <kir931028@gmail.com>
2025-08-09 14:42:58 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 92 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 'dev/stdin' : 'run/input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split(' ');
+
+console.log(parseInt(input, 10) ** 3);
