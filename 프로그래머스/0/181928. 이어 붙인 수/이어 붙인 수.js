aa13a1aae12bc4e321f238fef3981033c0cf8caa
CR Han <kir931028@gmail.com>
2025-08-15 13:17:39 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.13 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,13 @@
+function solution(num_list) {
+    let odd = even = ''
+    
+    for (num of num_list){
+        if(num%2 === 0){
+            even += String(num)
+        } else {
+            odd += String(num)
+        }
+    }
+    
+    return parseInt(odd, 10) + parseInt(even, 10);
+}
