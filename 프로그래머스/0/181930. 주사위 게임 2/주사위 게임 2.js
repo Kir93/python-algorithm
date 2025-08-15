5350079d9c84689e34672220e1dea4b450b30290
CR Han <kir931028@gmail.com>
2025-08-15 13:06:43 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,15 @@
+function sum(a, b, c){
+    return [a,b,c].reduce((acc, prev) => acc += prev)
+}
+
+function solution(a, b, c) {
+    let flag = [...new Set([a, b, c])].length
+    
+    if(flag === 3){
+        return sum(a, b, c)
+    }else if(flag === 2){
+        return sum(a, b, c) * sum(a**2, b**2, c**2)
+    }else{
+        return sum(a, b, c) * sum(a**2, b**2, c**2) * sum(a**3, b**3, c**3)
+    }
+}
