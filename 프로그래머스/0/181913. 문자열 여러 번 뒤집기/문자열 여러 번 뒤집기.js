d5637fc2f25a0ae956ffcf83e414a784e26868f7
CR Han <kir931028@gmail.com>
2025-08-19 14:33:25 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 21.23 ms, Memory: 34.9 MB -BaekjoonHub

--- 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
+++ 프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -1,9 +1,7 @@
 function solution(my_string, queries) {
   let list = my_string.split('');
-  for ([s, e] of queries) {
-    list = list
-      .slice(0, s)
-      .concat(list.slice(s, e + 1).reverse(), list.slice(e + 1));
-  }
+  queries.forEach(([s, e]) => {
+    list.splice(s, e - s + 1, ...list.slice(s, e + 1).reverse());
+  });
   return list.join('');
 }
