21cd422a396dd5c693ecaeaa8ac02182c8767da0
CR Han <kir931028@gmail.com>
2025-08-20 15:29:21 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.js
@@ -0,0 +1,11 @@
+function solution(my_string, s, e) {
+  return (
+    my_string.slice(0, s) +
+    my_string
+      .split('')
+      .slice(s, e + 1)
+      .reverse()
+      .join('') +
+    my_string.slice(e + 1)
+  );
+}
