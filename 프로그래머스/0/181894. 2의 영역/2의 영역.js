447a7eab878e953cb944c05089370fb784cd6b8c
CR Han <kir931028@gmail.com>
2025-08-23 19:11:20 +0900

    [level 0] Title: 2의 영역, Time: 0.48 ms, Memory: 42.4 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.js
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.js
@@ -1,7 +1,5 @@
 function solution(arr) {
   return arr.indexOf(2) === -1
     ? [-1]
-    : arr.indexOf(2) === arr.lastIndexOf(2)
-    ? arr.slice(arr.indexOf(2), arr.indexOf(2) + 1)
     : arr.slice(arr.indexOf(2), arr.lastIndexOf(2) + 1);
 }
