0619c68d2b8d74c80626963e197c701f02ed4c40
CR Han <kir931028@gmail.com>
2025-08-23 19:17:16 +0900

    [level 0] Title: 배열 조각하기, Time: 2.14 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.js
@@ -0,0 +1,11 @@
+function solution(arr, query) {
+  for (let i = 0; i < query.length; i++) {
+    if (i % 2 === 0) {
+      arr = arr.slice(0, query[i] + 1);
+    } else {
+      arr = arr.slice(query[i]);
+    }
+  }
+
+  return arr;
+}
